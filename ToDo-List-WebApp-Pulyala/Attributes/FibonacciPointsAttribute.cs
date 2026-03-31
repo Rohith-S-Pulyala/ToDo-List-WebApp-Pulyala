9a79c5d1b018eea685325ff191e9f6ddcf4b5451
Rohith Pulyala <rspulyala@dmacc.edu>
2026-03-30 20:58:46 -0500

    Added input validation for fields Name, Description, SprintNumber, and PointValue.

---
+++ ToDo-List-WebApp-Pulyala/Attributes/FibonacciPointsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo_List_WebApp_Pulyala.Attributes
+{
+    public class FibonacciPointsAttribute: ValidationAttribute
+    {
+        private static readonly int[] AllowedPts = { 0, 1, 2, 3, 5, 8, 13, 21 }; // Fixed Set of Points for validation
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is int point && Array.IndexOf(AllowedPts, point) != -1) // Input validation for one of the numbers in the array.
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage ?? "Invalid point value."); // Error message for failure.
+        }
+    }
+}
--- ToDo-List-WebApp-Pulyala/Models/Ticket.cs
+++ ToDo-List-WebApp-Pulyala/Models/Ticket.cs
@@ -1,12 +1,29 @@
-﻿namespace ToDo_List_WebApp_Pulyala.Models
+﻿using System.ComponentModel.DataAnnotations;
+using ToDo_List_WebApp_Pulyala.Attributes;
+
+namespace ToDo_List_WebApp_Pulyala.Models
 {
     public class Ticket
     {
         public int Id { get; set; } // Primary Key
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)] // Makes sure field is not null, minimum 5 characteristics.
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0, 999, ErrorMessage = "Sprint Number must be Non-Negative.")]
         public int SprintNumber { get; set; }
+
+        // Validation to make sure Administrator inputs all numbers.
+        [Required]
+        [FibonacciPoints(ErrorMessage = "Point value must be a valid Fibonacci number (1, 2, 3, 5, 8, 13, 21).")]
         public int PointValue { get; set; }
-        public string Status { get; set; } // TO Do, In Progress, QA, Done
+
+        // Validation not necessary, dropdown menu has a fixed set of options, none to add.
+        public string Status { get; set; } // To Do, In Progress, QA, Done
     }
 }
